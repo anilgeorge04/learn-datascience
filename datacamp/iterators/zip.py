9b6601b563a6ac91502a08a403a9284a825df6ec
anilg <62422148+anilgeorge04@users.noreply.github.com>
2021-03-12 05:44:59 +0530

    toolbox2 bring it together

--- datacamp/iterators/zip.py
+++ datacamp/iterators/zip.py
@@ -31,3 +31,49 @@ result1, result2 = zip(*z1)
 # Check if unpacked tuples are equivalent to original tuples
 print(result1 == mutants)
 print(result2 == powers)
+
+# Function to zip 2 lists and make a dictionary
+# Define lists2dict()
+def lists2dict(list1, list2):
+    """Return a dictionary where list1 provides
+    the keys and list2 provides the values."""
+
+    # Zip lists: zipped_lists
+    zipped_lists = zip(list1, list2)
+
+    # Create a dictionary: rs_dict
+    rs_dict = dict(zipped_lists)
+
+    # Return the dictionary
+    return rs_dict
+
+# Call lists2dict: rs_fxn
+rs_fxn = lists2dict(feature_names, row_vals)
+
+# Print rs_fxn
+print(rs_fxn)
+
+# Add a list comprehension to make list of dicts
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
+
+# Make this list of dicts a DataFrame
+# Import the pandas package
+import pandas as pd
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Turn list of dicts into a DataFrame: df
+df = pd.DataFrame(list_of_dicts)
+
+# Print the head of the DataFrame
+print(df.head())
